58a3d248c9312cb19913ff64e71e524efde18913
Lena <Derinhelm@yandex.ru>
2019-02-22 00:42:32 +0300

    Dark red triangle with shaders with in, out

--- prob2/shaders/fragment.glsl
+++ prob2/shaders/fragment.glsl
@@ -1,8 +1,9 @@
 #version 330 core
+in vec4 vertexColor; // Входная переменная из вершинного шейдера (то же название и тот же тип)
 
 out vec4 color;
 
 void main()
 {
-	color = vec4(1.0f, 0.5f, 0.2f, 1.0f);
-}
+    color = vertexColor;
+} 
