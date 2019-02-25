c67d2c650e8f5e8fc7e8511f306ccaaf5a89f1bd
Lena <Derinhelm@yandex.ru>
2019-02-25 23:49:14 +0300

    raymarching box with shift

--- my-prob/shaders/fragment.glsl
+++ my-prob/shaders/fragment.glsl
@@ -47,8 +47,8 @@ bool RayIntersection(vec3 ray_pos, vec3 ray_dir){
     int max_steps = 64;
     float mindist = 0.01f;
     for (int i = 0; i < max_steps; i++) {
-        float dist = sdSphere(ray_pos - vec3(0.7f, 0.7f, 0.7f), 1.0f);
-        //float dist = sdBox(ray_pos, vec3(0.5, 0.5, 0.5));
+        //float dist = sdSphere(ray_pos - vec3(0.7f, 0.7f, 0.7f), 1.0f);
+        float dist = sdBox(ray_pos - vec3(-0.9f, -0.9f, -0.9f), vec3(0.5, 0.5, 0.5));
         if (abs(dist) < mindist) {
             return true;
         }
